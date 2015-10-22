62d5aeb4227d4d1772563358624cd6521b824d00
Benson Mburu <nguruben@gmail.com>
2015-10-22 20:45:44 +0300

    contact form

---
+++ contact.php
@@ -0,0 +1,78 @@
+<!doctype html><html>
+
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+
+<head>
+<meta charset="utf-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+<title>CONTACT FORM</title>
+<meta name="description" content="Wanna talk to me about a web project or hire me for a freelance? Just fill the form below or send an email to rafael@caferati.me and let&#x27;s talk."/>
+<meta id="viewport" name="viewport" content="width=device-width,minimum-scale=0.8,maximum-scale=1,user-scalable=no"/>
+<link rel="stylesheet" href="css/main.css">
+
+<link async rel="import" href="build1896.html?v=59"/>
+<script>var wrap=null;document.head.createShadowRoot||document.write("<script src='js/webcomponents.min.js'><\/script>"),function(e,t,a,n,c,s,o){e.GoogleAnalyticsObject=c,e[c]=e[c]||function(){(e[c].q=e[c].q||[]).push(arguments)},e[c].l=1*new Date,s=t.createElement(a),o=t.getElementsByTagName(a)[0],s.async=1,s.src=n,o.parentNode.insertBefore(s,o)}(window,document,"script","../www.google-analytics.com/analytics.js","ga"),ga("create","UA-51484092-1","auto"),ga("require","displayfeatures"),ga("send","pageview");</script>
+<script src="js/custom.js"></script>
+</head>
+<body class="page-exit animating">
+<header id="page-header" class="hidden" data-title="Message| Benson Mburu">
+
+<nav>
+<a href="index.html"><span>about</span></a>
+<a href="work.html"><span>work</span></a>
+ <a href="labs.html"><span>labs</span></a>
+ <a href="contact.html"><span>contact</span></a>
+
+ </nav>
+ <div class="burger"><i></i> <i></i> <i></i></div>
+ </header>
+ <main id="page-content">
+ <section class="current">
+ <div>
+ <div class="page-control">
+ <a href="labs.html" class="prev">
+ <span>Labs</span>
+ <em></em></a></div>
+ <div class="content pages" id="contact">
+ <section class="centered">
+ <div class="wrapper">
+ <header>
+ <h1>Contact Me</h1>
+
+ </header>
+ <div class="lets-talk">
+ <form method="post" autocomplete="off" action="https://caferati.me/">
+ <ul class="input-block">
+ <li class="active name">
+ <div class="label cf_">
+ <i class="i-svg user" data-icon="user,"></i>
+ <label title="Name">Fill in your name</label></div>
+ <i class="i-svg common" data-icon="user,"></i>
+ <input autocomplete="off" type="text" name="name" placeholder="Fill with your name"/></li>
+ <li class="email"><div class="label cf_"><i class="i-svg mail" data-icon="envelope,"></i><label title="email">Please enter your email address</label></div>
+ <i class="i-svg common" data-icon="envelope,"></i>
+  <input autocomplete="off" type="text" name="email" placeholder="Please enter your email address"/></li>
+
+ <li class="msg">
+ <div class="label cf_">
+ <i class="i-svg msg" data-icon="edit,"></i>
+ <label title="message">Please Write Your Message</label></div>
+ <i class="i-svg common" data-icon="edit,"></i>
+ <textarea placeholder="now write your awesome message :)" name="message"></textarea><
+ /li></ul>
+ <div class="submit"><awesome-button color="teal" width="100" disabled="true">Next</awesome-button><awesome-button color="teal" disabled="true" progress="true">Submit your message</awesome-button></div>
+ <div class="status"></div>
+ </form>
+ <div class="share"><p>You can also find me on</p>
+ <awesome-button width="130" type="facebook" target="_blank" href="https://www.facebook.com/r.caferati">Facebook</awesome-button>
+ <awesome-button type="linkedin" width="130" target="_blank" href="https://www.linkedin.com/in/rcaferati">Linkedin</awesome-button>
+ <awesome-button type="twitter" width="120" target="_blank" href="https://twitter.com/rcaferati">Twitter</awesome-button>
+ <awesome-button type="github" width="120" target="_blank" href="https://github.com/rcaferati">Github</awesome-button></div>
+
+ </div></div></section></div></div></section></main>
+ <div id="page-loader" class="avatar-loader">
+ <div class="avatar">
+ <div class="avatar-wrapper">
+ <svg version="1.1" id="loading-avatar" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="512px" height="512px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve"><g class="circle"><path fill="#A5A5A5" d="M256,29.245C130.771,29.245,29.245,130.754,29.245,256c0,125.225,101.526,226.755,226.755,226.755 c125.229,0,226.755-101.53,226.755-226.755C482.755,130.754,381.229,29.245,256,29.245z"/></g><circle class="out" fill="none" stroke="#888888" stroke-width="30" stroke-miterlimit="10" cx="256" cy="256" r="222.127"/><g><circle class="fill" id="avatar-fill" fill="none" stroke="#292929" stroke-width="34" stroke-miterlimit="10" cx="256" cy="256" r="223.978"/><circle class="fill" fill="none" stroke="#888888" stroke-width="35" stroke-miterlimit="10" cx="256" cy="256" r="223.978"/><circle class="fill" fill="none" stroke="#292929" stroke-width="36" stroke-miterlimit="10" cx="256" cy="256" r="223.978"/></g></svg> <img class="ninja" alt="Avatar" src="../caferati-two-caferati.netdna-ssl.com/images/avatar.svg"/></div></div></div><script async data-require="false" data-main="https://caferati-two-caferati.netdna-ssl.com/scripts/build-59" src="js/require.js"></script><script>var SRT={};SRT.o=Date.now(),SRT.a=setTimeout(function(){SRT.r=document.getElementById("page-loader"),SRT.r=SRT.r.querySelectorAll(".avatar"),SRT.r[0].classList.add("active")},1e3),SRT.f=setTimeout(function(){SRT.s=Date.now(),SRT.d=900,SRT.r[0].classList.add("first")},1900),SRT.s=setTimeout(function(){SRT.s=Date.now(),SRT.d=1350,SRT.r[0].classList.contains("first")&&SRT.r[0].classList.add("second")},3250),SRT.t=setTimeout(function(){SRT.s=Date.now(),SRT.d=1350,SRT.r[0].classList.contains("second")&&SRT.r[0].classList.add("third")},4550);var DATA={scripts:[""],partials:["control"],filter:{list:[]},prev:{},next:{},template:"contact",index:4,name:"contact"};</script></body>
+
+</html>
