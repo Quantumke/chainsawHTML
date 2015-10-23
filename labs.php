0074fa7c32841c19e4bc9903c889a435f5a5d9e6
Benson Mburu <nguruben@gmail.com>
2015-10-23 14:07:17 +0300

    startted on labs

--- labs.php
+++ labs.php
@@ -40,11 +40,14 @@
  <h1>Contact Me</h1>
 
  </header>
+
  <div class="lets-talk">
- <form method="post" autocomplete="off" action="https://caferati.me/">
+
  <ul class="input-block">
+
  <li class="active name">
  <div class="label cf_">
+  <form method="post" autocomplete="off" action="https://caferati.me/">
  <i class="i-svg user" data-icon="user,"></i>
  <label title="Name">Fill in your name</label></div>
  <i class="i-svg common" data-icon="user,"></i>
@@ -58,21 +61,29 @@
  <i class="i-svg msg" data-icon="edit,"></i>
  <label title="message">Please Write Your Message</label></div>
  <i class="i-svg common" data-icon="edit,"></i>
- <textarea placeholder="now write your awesome message :)" name="message"></textarea><
- /li></ul>
+ <textarea placeholder="now write your awesome message :)" name="message"></textarea>
+ </li>
+  <input type="submit" name="submit" value="submit">
+  </form>
+ </ul>
  <div class="submit"><awesome-button color="teal" width="100" disabled="true">Next</awesome-button><awesome-button color="teal" disabled="true" progress="true">Submit your message</awesome-button></div>
+
  <div class="status"></div>
- </form>
+
  <div class="share"><p>You can also find me on</p>
  <awesome-button width="130" type="facebook" target="_blank" href="https://www.facebook.com/r.caferati">Facebook</awesome-button>
  <awesome-button type="linkedin" width="130" target="_blank" href="https://www.linkedin.com/in/rcaferati">Linkedin</awesome-button>
  <awesome-button type="twitter" width="120" target="_blank" href="https://twitter.com/rcaferati">Twitter</awesome-button>
  <awesome-button type="github" width="120" target="_blank" href="https://github.com/rcaferati">Github</awesome-button></div>
 
- </div></div></section></div></div></section></main>
+ </div></div></section></div></div>
+
+ </section></main>
  <div id="page-loader" class="avatar-loader">
  <div class="avatar">
  <div class="avatar-wrapper">
- <svg version="1.1" id="loading-avatar" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="512px" height="512px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve"><g class="circle"><path fill="#A5A5A5" d="M256,29.245C130.771,29.245,29.245,130.754,29.245,256c0,125.225,101.526,226.755,226.755,226.755 c125.229,0,226.755-101.53,226.755-226.755C482.755,130.754,381.229,29.245,256,29.245z"/></g><circle class="out" fill="none" stroke="#888888" stroke-width="30" stroke-miterlimit="10" cx="256" cy="256" r="222.127"/><g><circle class="fill" id="avatar-fill" fill="none" stroke="#292929" stroke-width="34" stroke-miterlimit="10" cx="256" cy="256" r="223.978"/><circle class="fill" fill="none" stroke="#888888" stroke-width="35" stroke-miterlimit="10" cx="256" cy="256" r="223.978"/><circle class="fill" fill="none" stroke="#292929" stroke-width="36" stroke-miterlimit="10" cx="256" cy="256" r="223.978"/></g></svg> <img class="ninja" alt="Avatar" src="../caferati-two-caferati.netdna-ssl.com/images/avatar.svg"/></div></div></div><script async data-require="false" data-main="https://caferati-two-caferati.netdna-ssl.com/scripts/build-59" src="js/require.js"></script><script>var SRT={};SRT.o=Date.now(),SRT.a=setTimeout(function(){SRT.r=document.getElementById("page-loader"),SRT.r=SRT.r.querySelectorAll(".avatar"),SRT.r[0].classList.add("active")},1e3),SRT.f=setTimeout(function(){SRT.s=Date.now(),SRT.d=900,SRT.r[0].classList.add("first")},1900),SRT.s=setTimeout(function(){SRT.s=Date.now(),SRT.d=1350,SRT.r[0].classList.contains("first")&&SRT.r[0].classList.add("second")},3250),SRT.t=setTimeout(function(){SRT.s=Date.now(),SRT.d=1350,SRT.r[0].classList.contains("second")&&SRT.r[0].classList.add("third")},4550);var DATA={scripts:[""],partials:["control"],filter:{list:[]},prev:{},next:{},template:"contact",index:4,name:"contact"};</script></body>
+ </div></div></div>
+ <script async data-require="false" data-main="https://caferati-two-caferati.netdna-ssl.com/scripts/build-59" src="js/require.js"></script>
+ <script>var SRT={};SRT.o=Date.now(),SRT.a=setTimeout(function(){SRT.r=document.getElementById("page-loader"),SRT.r=SRT.r.querySelectorAll(".avatar"),SRT.r[0].classList.add("active")},1e3),SRT.f=setTimeout(function(){SRT.s=Date.now(),SRT.d=900,SRT.r[0].classList.add("first")},1900),SRT.s=setTimeout(function(){SRT.s=Date.now(),SRT.d=1350,SRT.r[0].classList.contains("first")&&SRT.r[0].classList.add("second")},3250),SRT.t=setTimeout(function(){SRT.s=Date.now(),SRT.d=1350,SRT.r[0].classList.contains("second")&&SRT.r[0].classList.add("third")},4550);var DATA={scripts:[""],partials:["control"],filter:{list:[]},prev:{},next:{},template:"contact",index:4,name:"contact"};</script></body>
 
 </html>
